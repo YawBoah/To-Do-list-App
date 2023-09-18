728f374c94386627a4f048f6ba0d8017aca66e3e
YBoah <yawboahene.yb@gmail.com>
2023-09-18 02:26:08 +0000

    Added the save functionality in the Js file

--- script.js
+++ script.js
@@ -14,13 +14,25 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
